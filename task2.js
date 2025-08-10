c244faeb08bf321962adc8447963557a1925214a
roksanadilshad <tanvirhossaintufa@gmail.com>
2025-08-10 23:01:48 +0600

    task2

---
+++ task2.js
@@ -0,0 +1,15 @@
+function repeatedItem(numbers, find){
+    let arr = 0;
+    for(const num of numbers){
+        if(numbers.includes(find) && num === find){
+            arr++;
+        }
+    }
+    return arr;
+}
+const numbers =  [5,6,11,12,98, 5];
+let find = 5;
+console.log(repeatedItem(numbers, find));
+const number =  [5,6,11,12,98, 5];
+let fin = 25;
+console.log(repeatedItem(number, fin));
